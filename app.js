cacc9f9713ebe059e29e100f5138b0e598df5acf
atakan yontar <atakanyontar@gmail.com>
2022-02-20 16:51:07 +0300

    Edited views pages

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const Post = require('./models/Post')
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -12,6 +20,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+)
 
 app.get('/', async (req, res) => {
   const posts = await Post.find({})
@@ -39,6 +52,24 @@ app.post('/posts', async (req,res) => {
   console.log(req.body);
   Post.create(req.body);
   res.redirect('/')
+})
+
+app.get('/posts/edit/:id', async (req,res) => {
+  const post = await Post.findOne({ _id: req.params.id})
+  res.render('edit',{
+    post
+  })
+})
+
+app.put('/posts/:id', async (req, res) => {
+  const post = await Post.findOne({_id: req.params.id});
+  post.title = req.body.title;
+  console.log(req.body.title);
+  post.detail = req.body.detail;
+  console.log(req.body.detail);
+  post.save();
+
+  res.redirect(`/post/${req.params.id}`);
 })
 
 const port = 4000;
